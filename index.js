cd04beaca233a30135936b18749e17f7df6e7106
LazyCat <68156188+LazyCat2@users.noreply.github.com>
2022-09-04 20:24:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const
         ws: { 
             properties: {
                     $os: process.platform,
-                    $browser: "Discord Android",
+                    $browser: "Discord Android", //для статуса "в сети с телефона"
                     $device: "discord.js" 
                 }
             }
     }),
-    db = createClient(
+    db = createClient( //База данных
         process.env.db_url,
         process.env.db_key
     );
 
 module.exports = {bot: bot, startTime: Math.round(Date.now()/1000), db: db}
-const { handleError, getLocalisation, refreshGuild } = require('./utils.js')
-black_list = []
-if (process.argv.includes('--refresh-slash')) {
-    require('./slash/init.js');
+const { handleError, getLocalisation, refreshGuild } = require('./utils.js') // оно не на верху ибо должно быть импортировано ДО создания module.exports
+
+if (process.argv.includes('--refresh-slash')) { //если запущено через node . --refrash-slash
+    require('./slash/init.js');                                 //то обновляем слеш команды
 }
 
 interactionTypes = [
@@ -33,20 +33,20 @@ interactionTypes = [
 ];
 selectMenus = {};
 
-console.log('')
-start = Date.now();
+console.log('') // \n
+start = Date.now(); //смотрим за сколько времени запустится бот
 bot.on('ready', async ()=>{
-    await bot.user.setPresence({ activities: [{ name: 'поедании вискаса', type: 5 }]});
-    duration = Date.now() - start;
-    bot.guilds.cache.each(g=>{ refreshGuild(g) })
+    await bot.user.setPresence({ activities: [{ name: 'поедании вискаса', type: 5 }]}); //статус бота
+    duration = Date.now() - start; 
+    bot.guilds.cache.each(g=>{ refreshGuild(g) }) //обновляем сервера в бд
     console.log(`${bot.user.username} is ready for ${duration}ms`);
 })
 
 bot.on('interactionCreate', async (inter)=>{
-    data = (await db
+    data = (await db //получаем данные из сервера
         .from('servers')
         .select('*')
-        .match({ id: inter.guildId })).data[0]
+        .match({ id: inter.guildId })).data[0] 
 
     path = './'
     try {
